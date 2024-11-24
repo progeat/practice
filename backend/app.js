0f34bef880dd119fa10ad1ee87db81bfc2491e81
Andrey Khokhlov <progeat@mail.ru>
2024-11-24 23:07:28 +0300

    Настройка docker

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
