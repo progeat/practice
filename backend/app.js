ce4690045f1a2379fb38f21e39a841d3617772ff
Andrey Khokhlov <progeat@mail.ru>
2024-11-24 21:42:07 +0300

    fix backend

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,16 @@ const routes = require('./routes');
 const port = 3001;
 const app = express();
 
-app.use(express.static('../frontend/build'));
+app.use(express.static(path.resolve('..', 'frontend', 'build')));
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('..', 'frontend', 'build', 'index.html'));
+});
 
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
   app.listen(port, () => {
